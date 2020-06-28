d67973c4a13271f672060c61e9e9771d67509b73
Kousuke Kawahira <kawahira@binzume.net>
2020-06-28 20:30:32 +0900

    remove http server. also...

--- DoorKeeper/DoorKeeper.ino
+++ DoorKeeper/DoorKeeper.ino
@@ -1,6 +1,5 @@
 #include <ESP8266WiFi.h>
 #include <WiFiClient.h>
-#include <ESP8266WebServer.h>
 #include <ESP8266mDNS.h>
 #include <Servo.h> 
 #include <EEPROM.h>
@@ -8,53 +7,44 @@
 #include <Ticker.h>
 #include "config.h"
 
+#define LOG Serial
+
+// Wio Node
+const uint8_t PORT0A = 1;
+const uint8_t PORT0B = 3;
+const uint8_t PORT1A = 4;
+const uint8_t PORT1B = 5;
+const uint8_t PORT_POWER = 15;
+const uint8_t FUNC_BTN = 0;
+const uint8_t BLUE_LED = 2;
+
+// servo params.
+const uint8_t lockServoPin = PORT1A;
+const uint8_t waitServoPos = 90;
+const uint8_t lockServoPos = 5;
+const uint8_t unlockServoPos = 175;
+const bool lockOnPowerOn = false;
+
 MDNSResponder mdns;
-ESP8266WebServer server(80);
 Servo lockServo;
 Ticker ticker;
 WiFiUDP udpServer;
 Ticker pingTicker;
 
-const uint8_t led = 4;
+bool locked = lockOnPowerOn;
 
-const uint8_t lockServoPos = 90;
-const uint8_t unlockServoPos = 180;
-
-const uint8_t lockServoPin = 13;
-const bool lockOnPowerOn = true;
-bool locked = true;
-
-bool pingReady = false;
-
-void handleRoot() {
-  digitalWrite(led, 1);
-  server.send(200, "text/plain", "Door keeper v0.1");
-  digitalWrite(led, 0);
-}
-
-void handleNotFound(){
-  digitalWrite(led, 1);
-  String message = "File Not Found\n\n";
-  message += "URI: ";
-  message += server.uri();
-  message += "\nMethod: ";
-  message += (server.method() == HTTP_GET)?"GET":"POST";
-  message += "\nArguments: ";
-  message += server.args();
-  message += "\n";
-  for (uint8_t i=0; i<server.args(); i++){
-    message += " " + server.argName(i) + ": " + server.arg(i) + "\n";
-  }
-  server.send(404, "text/plain", message);
-  digitalWrite(led, 0);
-}
- 
 void setup(void){
-  pinMode(led, OUTPUT);
-  digitalWrite(led, 0);
+  pinMode(BLUE_LED, OUTPUT);
+  pinMode(PORT_POWER, OUTPUT);
+  digitalWrite(BLUE_LED, 1);
+
+  pinMode(lockServoPin, OUTPUT);
+  digitalWrite(lockServoPin, 0);
+
   Serial.begin(115200);
+  WiFi.mode(WIFI_STA);
   WiFi.begin(ssid, password);
-  Serial.println("");
+  LOG.println("");
 
   if (lockOnPowerOn) {
     servo_apply();
@@ -63,73 +53,63 @@ void setup(void){
   // Wait for connection
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
-    Serial.print(".");
+    LOG.print(".");
   }
-  Serial.println("");
-  Serial.print("Connected to ");
-  Serial.println(ssid);
-  Serial.print("IP address: ");
-  Serial.println(WiFi.localIP());
+  LOG.println("");
+  LOG.print("Connected to ");
+  LOG.println(ssid);
+  LOG.print("IP address: ");
+  LOG.println(WiFi.localIP());
   
-  if (mdns.begin("esp8266", WiFi.localIP())) {
-    Serial.println("MDNS responder started");
+  if (mdns.begin("door1", WiFi.localIP())) {
+    LOG.println("MDNS responder started");
   }
-  
-  server.on("/", handleRoot);
-  
-  server.on("/door/status", [](){
-    ping();
-    server.send(200, "text/json", String("{\"status\":\"ok\", \"locked\": ") + (locked?"true":"false") +"}");
-  });
 
-  server.on("/door/lock", [](){
-    locked = true;
-    servo_apply();
-    server.send(200, "text/json", "{\"status\":\"ok\", \"message\": \"locked!\"}");
-  });
-
-  server.on("/door/unlock", [](){
-    locked = false;
-    servo_apply();
-    server.send(200, "text/json", "{\"status\":\"ok\", \"message\": \"unlocked!\"}");
-  });
-
-  server.onNotFound(handleNotFound);
-  
-  server.begin();
-  Serial.println("HTTP server started");
+  LOG.println("HTTP server started");
   udpServer.begin(apiPort);
-  Serial.println("UDP server started");
+  LOG.println("UDP server started");
   ping();
-  pingTicker.attach(600, ping);
+  pingTicker.attach(300, ping);
+
+  wifi_set_sleep_type(LIGHT_SLEEP_T);
 }
 
 void ping() {
-  pingReady = true;
-}
-
-void do_ping() {
-  Serial.println("ping");
+  LOG.println("ping");
   udpServer.beginPacket(apiHost, apiPort);
-  udpServer.write((String() + "{\"type\":\"door\",\"device\":\"door01\",\"token\":\""+apiToken+"\",\"locked\": " + (locked?"true":"false") +"}").c_str());
+  udpServer.write(
+      (String() + "type:door\tdevice:" + device + "\ttoken:" + apiToken+ "\tlocked:" + (locked?"true":"false"))
+          .c_str());
   udpServer.endPacket();
-  udpServer.flush();
 }
 
 void servo_apply() {
+  wifi_set_sleep_type(MODEM_SLEEP_T);
+  digitalWrite(lockServoPin, 0);
+  digitalWrite(PORT_POWER, 1);
+
   lockServo.attach(lockServoPin);
   lockServo.write(locked ? lockServoPos : unlockServoPos);
-  ticker.attach(3, servo_off); // servo off after 3 seconds.
+
+  ticker.attach(1, servo_newtral); // servo off after 3 seconds.
 }
- 
+
+void servo_newtral() {
+  ticker.detach();
+  lockServo.write(waitServoPos);
+  ticker.attach(1, servo_off); // servo off.
+}
+
 void servo_off() {
   lockServo.detach();
   ticker.detach();
+  digitalWrite(PORT_POWER, 0);
+  digitalWrite(lockServoPin, 0);
+  wifi_set_sleep_type(LIGHT_SLEEP_T);
 }
 
 void on_recv_event(String msg) {
-  Serial.println(msg);
-  // TODO auth
+  LOG.println(msg);
   if (msg == "token:"+apiToken+"\tcommand:lock") {
     locked = true;
     servo_apply();
@@ -137,23 +117,20 @@ void on_recv_event(String msg) {
     locked = false;
     servo_apply();
   } else {
-    Serial.println("unknown msg.");
+    LOG.println("unknown msg.");
   }
 }
 
 void loop(void){
-  server.handleClient();
   int packetSize = udpServer.parsePacket();
-  if (pingReady) {
-    pingReady = false;
-    do_ping();
-  }
   if (packetSize) {
     char packetBuffer[255];
     int len = udpServer.read(packetBuffer, sizeof(packetBuffer));
     if (len > 0) packetBuffer[len] = 0;
+    // LOG.println(packetSize);
     udpServer.flush();
     on_recv_event(packetBuffer);
+  } else {
+    delay(500);
   }
 }
-
